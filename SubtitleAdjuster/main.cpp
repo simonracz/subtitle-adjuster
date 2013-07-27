57d76dff415688efbe36176b0fc0ddb7a35477a6
Simon Racz <simonracz@gmail.com>
2013-07-27 20:15:49 +0200

    Initial Commit

---
+++ SubtitleAdjuster/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SubtitleAdjuster
+//
+//  Created by Simon Racz on 7/27/13.
+//  Copyright (c) 2013 Simon Racz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
